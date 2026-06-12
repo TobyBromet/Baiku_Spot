e12dd1d8d7f417781eb93c6b26b1bdc935ab33f0
Tobias Bromet <tobybromet@gmail.com>
2026-06-12 16:17:50 +0900

    Added ParkingSpot model data validations.

--- app/models/parking_spot.rb
+++ app/models/parking_spot.rb
@@ -1,2 +1,8 @@
 class ParkingSpot < ApplicationRecord
+  validates :address, presence: true, uniqueness: true
+  validates :longitude, presence: true, numericality: true
+  validates :latitude, presence: true, numericality: true
+  validates :supports_over_125cc, presence: true
+  validates :source_url, presence: true, uniqueness: true
+  validates :reservation_required, presence: true
 end
